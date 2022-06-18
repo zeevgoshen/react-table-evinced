feb8f0f2a008bfe56a2febb1ea157f545f021161
Ze'ev Goshen <zeevgoshen@gmail.com>
2022-06-18 11:59:38 +0300

    using axios to fetch data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { useState, createContext } from 'react';
-import Table from './components/table/table';
-import tabledata from './table.data'
-import './style.css';
+import React, { useState, createContext, useEffect } from "react";
+import Table from "./components/table/table";
+import axios from "axios";
+import "./style.css";
 
 export const IssuesContext = createContext();
 
 export default function App() {
+  const [issues, setIssues] = useState([]);
 
-  const [issues, setIssues] = useState(tabledata);
+  useEffect(() => {
+    loadIssuesData();
+  }, []);
+
+  const loadIssuesData = async () => {
+    await axios
+      .get("http://localhost:3000/issuesArray")
+      .then((response) => setIssues(response.data))
+      .catch((err) => console.log(err));
+  };
 
   return (
     <IssuesContext.Provider value={{ issues, setIssues }}>
       <div>
-        <Table data={ issues } loadIssues={setIssues} />
+        <Table data={issues} loadIssues={setIssues} />
       </div>
     </IssuesContext.Provider>
   );
--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import TableHeaders from './table.headers';
-import './table.css';
+import React from "react";
+import TableHeaders from "./table.headers";
+import "./table.css";
 
 const Table = (props) => {
-
   return (
-    <div>
-      <table style={{ width: 'auto', height: 'auto' }}>
-        <TableHeaders headernames={props.data} />
-        <tbody>
-          {props.data && props.data.map((obj, trindex) => (
+    <table>
+      <TableHeaders headernames={props.data} />
+      <tbody>
+        {props.data &&
+          props.data.map((obj, trindex) => (
             <tr key={trindex} className="trSeparator">
-              {obj && Object.values(obj).map((value, tdindex) => (
-                <td className="columnCell" key={tdindex}>
-                  {value}
-                </td>
-              ))}
+              {obj &&
+                Object.values(obj).map((value, tdindex) => (
+                  <td className="columnCell" key={tdindex}>
+                    {value}
+                  </td>
+                ))}
             </tr>
           ))}
-        </tbody>
-      </table>
-    </div>
+      </tbody>
+    </table>
   );
 };
 export default Table;
